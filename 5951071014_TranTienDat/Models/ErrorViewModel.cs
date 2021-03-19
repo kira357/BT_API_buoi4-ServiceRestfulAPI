c91962063d3453111b5bb0d57b97eda5980cd151
kira357 <dattrantd7@gmail.com>
2021-03-19 17:17:14 +0700

    Add project files.

---
+++ 5951071014_TranTienDat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _5951071014_TranTienDat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
